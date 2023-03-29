a56dc120858275e87b5b634432c431ed36d70654
Folka2134 <folka2134@gmail.com>
2023-03-29 12:48:33 +0100

    created second card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 
 import danielAvatar from "./assets/image-daniel.jpg";
+import jonathanAvatar from "./assets/image-jonathan.jpg";
 import quotationBg from "./assets/bg-pattern-quotation.svg";
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
       {/* GRID */}
       <div className="grid gap-4 grid-cols-1 grid-rows-none xl:grid-cols-4 xl:grid-rows-2 mt-20 xl:mt-0 mx-10">
         {/* CARD */}
-        <div className="flex flex-col bg-purple-600 xl:col-span-2 p-8 rounded-xl relative max-w-xl">
+        <div className="flex flex-col bg-purple-600 xl:col-span-2 p-8 rounded-xl relative ">
           <img className="absolute right-20 z-0" src={quotationBg} alt="" />
           <div className="flex items-center z-10">
             <img
@@ -21,7 +22,7 @@ function App() {
               <p className="text-sm font-semibold text-gray-200">
                 Daniel Clifford
               </p>
-              <p className="text-xs font-semibold text-gray-200 text-opacity-70">
+              <p className="text-xs font-semibold text-gray-200 text-opacity-50">
                 Verified Graduate
               </p>
             </div>
@@ -31,7 +32,7 @@ function App() {
             current, if not more so, in the company I joined. I honestly feel I
             got every penny's worth.
           </h4>
-          <p className="text-sm text-white text-opacity-70 font-semibold">
+          <p className="text-sm text-gray-200 text-opacity-70 font-semibold">
             “ I was an EMT for many years before I joined the bootcamp. I've
             been looking to make a transition and have heard some people who had
             an amazing experience here. I signed up for the free intro course
@@ -41,7 +42,31 @@ function App() {
             as a Software Engineer at a VR startup. ”
           </p>
         </div>
-        <div className="bg-green-400">2</div>
+        <div className="flex flex-col bg-[#48556A] p-8 rounded-xl relative">
+          <div className="flex items-center z-10">
+            <img
+              className="h-10 w-10 border-2 border-white border-opacity-40 mr-5 rounded-full"
+              src={jonathanAvatar}
+              alt="avatar"
+            />
+            <div>
+              <p className="text-sm font-semibold text-gray-200">
+                Jonathan Walters
+              </p>
+              <p className="text-xs font-semibold text-gray-200 text-opacity-70">
+                Verified Graduate
+              </p>
+            </div>
+          </div>
+          <h4 className="text-xl text-white font-semibold py-4 z-10">
+            The team was very supportive and kept me motivated
+          </h4>
+          <p className="text-sm text-white text-opacity-70 font-semibold">
+            “ I started as a total newbie with virtually no coding skills. I now
+            work as a mobile engineer for a big company. This was one of the
+            best investments I’ve made in myself. ”
+          </p>
+        </div>
         <div className="bg-blue-400 xl:row-span-2">3</div>
         <div className="bg-red-400">4</div>
         <div className="bg-yellow-400 xl:col-span-2">5</div>
